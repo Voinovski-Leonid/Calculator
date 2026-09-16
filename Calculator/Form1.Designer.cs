016c73ceaefd1667ee0bb2a63f42622923000341
Voinovski-Leonid <l.voinovski.4192@vallauri.edu>
2026-09-16 11:15:51 +0200

    creazione dinamica bottoni

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    partial class Form1
+    partial class Calcolatrice
     {
         /// <summary>
         /// Variabile di progettazione necessaria.
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Calcolatrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(322, 501);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Calcolatrice";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Calcolatrice_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -10,11 +10,51 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class Calcolatrice : Form
     {
-        public Form1()
+
+        private char[,] buttons =
+        {
+            { '%', '\u0152', 'C', '\u232B' },
+            { '\u215F', '\u00B2', '\u221A', '\u00F7' },
+            { '7', '8', '9', 'x' },
+            { '4', '5', '6', '-' },
+            { '1', '2', '3', '+' },
+            { '\u00B1', '=', ',', '=' }
+        };
+        public Calcolatrice()
         {
             InitializeComponent();
+        }
+
+        private void Calcolatrice_Load(object sender, EventArgs e)
+        {
+            makebuttons();
+        }
+
+        private void makebuttons()
+        {
+            int btnWidth = 80;
+            int btnHeight = 60;
+            int posY = 135;
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                int posX = 0;
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+                    Button btn = new Button();
+                    btn.Width = btnWidth;
+                    btn.Height = btnHeight;
+                    btn.Top = posY;
+                    btn.Left = posX;
+                    btn.Font = new Font("Seogoe UI", 16);
+                    btn.Text = buttons[i, j].ToString();
+                    btn.BackColor = Color.White;
+                    Controls.Add(btn);
+                    posX += btnWidth;
+                }
+                posY += btnHeight;
+            }
         }
     }
 }
